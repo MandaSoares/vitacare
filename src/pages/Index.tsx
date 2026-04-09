69474324f1eaf9c63f14d20484c5d4fb7533b19e
NTanzi <nathan.tanzi12345@gmail.com>
2026-04-08 22:09:55 -0300

    Arrumando index com novas funcionalidades

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,9 @@ import FeaturesSection from "@/components/landing/FeaturesSection";
 import HeroSection from "@/components/landing/HeroSection";
 import Navbar from "@/components/landing/Navbar";
 import NutritionistsSection from "@/components/landing/NutritionistSection";
+import Footer from "@/components/landing/Footer";
+import TestimonialsSection from "@/components/landing/TestimonialsSection";
+import CTASection from "@/components/landing/CTASection";
 
 const Index = () => {
   return (
@@ -12,6 +15,9 @@ const Index = () => {
       <FeaturesSection />
       <HowItWorksSection />
       <NutritionistsSection />
+      <TestimonialsSection />
+      <CTASection />
+      <Footer />
     </div>
   );
 };
