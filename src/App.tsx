be1e48e5e4f9be993862f774735a37a9fed6356b
MandaSoares <amanda_silvasoares@hotmail.com>
2026-05-11 13:33:19 -0300

    feat: adicionar recuperar senha e lembrar de mim no login

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Dashboard.tsx";
 import Index from "./pages/Index.tsx";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
+import ForgotPassword from "./pages/ForgotPassword.tsx";
 import NutritionistSearch from "./pages/NutritionistSearch.tsx";
 import PatientSearch from "./pages/PatientSearch.tsx";
 import PlanCreator from "./pages/PlanCreator.tsx";
@@ -35,6 +36,7 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/register" element={<Register />} />
             <Route path="/nutritionists" element={<NutritionistSearch />} />
             <Route
